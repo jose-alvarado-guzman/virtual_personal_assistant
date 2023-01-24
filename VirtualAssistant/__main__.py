1c90494de2d4c6b81f078d8b670f30b4c284282e
Jose A Alvarado-Guzman <jaa2220@columbia.edu>
2023-01-24 13:15:34 -0500

    Added the dialect feature

--- VirtualAssistant/__main__.py
+++ VirtualAssistant/__main__.py
@@ -3,18 +3,26 @@ import yaml
 import speech_recognition as sr
 from VirtualAssistant.SenseCells.tts import speak
 from VirtualAssistant.SenseCells.stt import listen
+from VirtualAssistant.dialect import Assistant
 
-with open('profile.yaml') as config_file:
+with open('profile.yaml', encoding='utf8') as config_file:
     profile_data = yaml.safe_load(config_file)
 
 name = profile_data['name']
 city_name = profile_data['city_name']
+assistant_name = profile_data['virtual_assistant_name']
 
 if __name__ == '__main__':
-    speak(f'Hi {name}, how can I help you today?')
-    stt = listen()
-    print(f'Results: {stt}')
-    if not 'error' in stt:
-        speak(f'Did you said, {stt}')
-    else:
-        speak('Sorry I have trouble understanding you')
+    virtual_assistant = Assistant(assistant_name)
+#    speak(f'Hi {name}, how are you today?')
+#    stt = listen()
+    while True:
+        query = input('> ')
+        if query in ['quit','bye']:
+            break
+        response = virtual_assistant.get_response(query)
+        print(response)
+#    if not 'error' in stt:
+#        speak(response)
+#    else:
+#        speak('Sorry I have trouble understanding you')
---
+++ VirtualAssistant/dialect.py
@@ -0,0 +1,37 @@
+import yaml
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
+
+class Assistant():
+
+    def __init__(self, name):
+        self.name = name
+        self.train_conversation()
+
+    def train_conversation(self):
+        self.chatbot=ChatBot(self.name, logic_adapters=[
+            'chatterbot.logic.BestMatch',
+            'chatterbot.logic.TimeLogicAdapter'])
+        corpus_trainer = ChatterBotCorpusTrainer(self.chatbot)
+        list_trainer = ListTrainer(self.chatbot)
+        list_trainer.train(["What is your name?",
+                            f'My name is {self.name}'
+                            "What's your name?",
+                            f'My name is {self.name}',
+                            'How should I call you',
+                            f'You can call me {self.name}',
+                            'Do you have a name',
+                            f'My name is {self.name}',
+                           "I need your assistance",
+                           "How can I help you today?",
+                           "Goodbye Jarvis",
+                           "Goodbye",
+                           "Goodbye",
+                           "Goodbye"
+                           ])
+        corpus_trainer.train("chatterbot.corpus.english.greetings",
+                      "chatterbot.corpus.english.conversations",
+                      "chatterbot.corpus.english.botprofile")
+
+    def get_response(self, message):
+        return self.chatbot.get_response(message)
